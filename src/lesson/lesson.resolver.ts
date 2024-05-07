6bab3551d67936cdfafdfcda4f6722665f83d619
anton1dev <anton.kuchmasov.dev@gmail.com>
2024-05-07 22:54:18 +0300

    initial commit

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,24 @@
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { LessonType } from './lesson.type';
+import { LessonService } from './lesson.service';
+import { CreateLessonInput } from './lesson.input';
+
+@Resolver(() => LessonType)
+export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+
+  @Query(() => [LessonType])
+  lessons() {
+    return this.lessonService.getLessons();
+  }
+
+  @Query(() => LessonType)
+  lesson(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
+  }
+
+  @Mutation(() => LessonType)
+  createLesson(@Args('createLessonInput') createLesson: CreateLessonInput) {
+    return this.lessonService.createLesson(createLesson);
+  }
+}
