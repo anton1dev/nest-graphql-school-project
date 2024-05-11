5a836c08999686940ea1cd8859b298094470526b
anton1dev <anton.kuchmasov.dev@gmail.com>
2024-05-11 14:58:20 +0300

    updated .env content

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,20 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import dotenv from 'dotenv';
 
 import { LessonModule } from './lesson/lesson.module';
 import { Lesson } from './lesson/lesson.entity';
 import { StudentModule } from './student/student.module';
 import { Student } from './student/student.entity';
 
+dotenv.config();
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mongodb',
-      url: 'mongodb://localhost/school',
+      url: process.env.MONGO_DB_STRING,
       synchronize: true,
       useUnifiedTopology: true,
       entities: [Lesson, Student],
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
